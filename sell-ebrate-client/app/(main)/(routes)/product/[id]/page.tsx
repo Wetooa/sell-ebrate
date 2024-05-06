da780926ebb7328410a147a7502d77c4145aa2a7
wetooa <derpykidyt@gmail.com>
2024-05-06 10:04:02 +0800

    removed seed, needs changes, addded a get useGetProduct logic

--- sell-ebrate-client/app/(main)/(routes)/product/[id]/page.tsx
+++ sell-ebrate-client/app/(main)/(routes)/product/[id]/page.tsx
@@ -1,25 +1,43 @@
 "use client";
+
 import { serverDomain } from "@/util/server";
 import { Product } from "@/util/types";
 import axios from "axios";
 import { useRouter, useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+function useGetProduct(productId: string) {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const { data } = await axios.get(serverDomain + `product/single`, {
+        productId
+      } as any);
+
+      fetchProfile();
+    };
+    fetchProfile();
+  }, []);
+
+  return product;
+}
 
 export default function ProductPage() {
   const { id } = useParams();
 
 
-  // FIX: bruh dumbo man diay ni nga idea
-  // const { data } = await axios.get(serverDomain + `product/{id}`);
-  // const { productName, description, quantity, price } = data as Product;
+  if (!id) {
+    return <></>
+  }
 
+  const product = useGetProduct(id as string);
 
   // TODO: logic where it grabs data about that product as well as comments, rating, and stuff
 
 
-
   return (
-    <div>Product hwerwerwer</div>
+    <div>{product}</div>
   )
 }
 
