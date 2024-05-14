b633e186fa9fc4b318bff57282cd39d22bf902ee
Ceven <valmykelcevenbolante@gmail.com>
2024-05-14 22:47:29 +0800

    try daw ko test gi change nakong logic gamay

--- sell-ebrate-server/routes/product/search/index.php
+++ sell-ebrate-server/routes/product/search/index.php
@@ -2,24 +2,35 @@
 
 include_once "../../../utils/headers.php";
 
+
 switch ($_SERVER["REQUEST_METHOD"]) {
   case "GET":
     $searchKeyword = $_GET['searchKeyword'] ?? null;
 
-    // FIX: not working on my end
-    if ($searchKeyword) {
-      $sqlGetProducts = "SELECT * FROM tblProduct WHERE MATCH(productName, description) AGAINST (? IN NATURAL LANGUAGE MODE) ORDER BY clicks LIMIT 10";
-      $stmt = $conn->prepare($sqlGetProducts);
-      $stmt->bind_param("s", $searchKeyword);
-    } else {
-      $sqlGetProducts = "SELECT * FROM tblProduct ORDER BY clicks LIMIT 10";
-      $stmt = $conn->prepare($sqlGetProducts);
+    try {
+        if ($searchKeyword) {
+          $sqlGetProducts = "SELECT * FROM tblProduct WHERE MATCH(productName, description) AGAINST (? IN NATURAL LANGUAGE MODE) ORDER BY clicks LIMIT 10";
+          $stmt = $conn->prepare($sqlGetProducts);
+          $stmt->bind_param("s", $searchKeyword);
+        } else {
+          $sqlGetProducts = "SELECT * FROM tblProduct ORDER BY RAND() LIMIT 10";
+          $stmt = $conn->prepare($sqlGetProducts);
+        }
+
+      $stmt->execute();
+      $result = $stmt->get_result();
+      $products = $result->fetch_all(MYSQLI_ASSOC);
+
+      $response = new ServerResponse(data: ["message" => "Products data fetched successfully", "products" => $products]);
+      returnJsonHttpResponse(200, $response);
+    } catch (Exception $e) {
+      $response = new ServerResponse(data: ["message" => "An error occurred: " . $e->getMessage()]);
+      returnJsonHttpResponse(500, $response);
     }
+    break;
 
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $products = $result->fetch_all(MYSQLI_ASSOC);
-
-    $response = new ServerResponse(data: ["message" => "Products data fetched successfully", "products" => $products]);
-    returnJsonHttpResponse(200, $response);
+  default:
+    $response = new ServerResponse(data: ["message" => "Invalid request method"]);
+    returnJsonHttpResponse(405, $response);
+    break;
 }
