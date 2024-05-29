5f91bce85b432ed7d577074bd87b50c6b0a43e4b
Ceven <valmykelcevenbolante@gmail.com>
2024-05-29 22:25:37 +0800

    small things before gone

--- sell-ebrate-server/routes/cart/index.php
+++ sell-ebrate-server/routes/cart/index.php
@@ -1,21 +1,22 @@
-
-
 <?php
 include_once "../../utils/headers.php";
-
 
 switch ($_SERVER["REQUEST_METHOD"]) {
   case "GET":
     $token = getAuthPayload();
     $accountId = $token["accountId"];
 
-    $sql = "SELECT * FROM tblCart AS a JOIN tblProduct AS b ON a.productId = b.productId WHERE userId = ? ";
+    $sql = "SELECT b.productId, b.productName, b.productDescription, b.productPrice, COUNT(a.productId) as quantity
+            FROM tblCart AS a
+            JOIN tblProduct AS b ON a.productId = b.productId
+            WHERE a.userId = ?
+            GROUP BY b.productId, b.productName, b.productDescription, b.productPrice";
     $result = $conn->execute_query($sql, [$accountId]);
     $cart = $result->fetch_all(MYSQLI_ASSOC);
 
-
     $response = new ServerResponse(data: ["message" => "User cart fetched successfully", "cart" => $cart]);
     returnJsonHttpResponse(200, $response);
+    break;
 
   case "POST":
     $payload = getBodyParameters();
@@ -26,10 +27,12 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
     $response = new ServerResponse(data: ["message" => "Cart item added successfully"]);
     returnJsonHttpResponse(200, $response);
+    break;
 
-
-
-  case "UPDATE":
+    case "UPDATE":
+      //i dunno the logic pa if mag update ko sa tbl kay wa siyay quantity or 
+      // just msg me 
+      break;
 
   case "DELETE":
     $payload = getAuthPayload();
@@ -38,7 +41,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
     $productId = $jsonData["productId"];
     $userId = $payload["accountId"];
 
-    $sqlDeleteCartItem = $conn->prepare("DELETE FROM tblCartItem WHERE cartId IN (SELECT cartId FROM tblCart WHERE userId = ?) AND productId = ?");
+    $sqlDeleteCartItem = $conn->prepare("DELETE FROM tblCart WHERE userId = ? AND productId = ?");
     $sqlDeleteCartItem->bind_param("ii", $userId, $productId);
     $sqlDeleteCartItem->execute();
 
@@ -49,4 +52,6 @@ switch ($_SERVER["REQUEST_METHOD"]) {
       $response = new ServerResponse(error: ["message" => "Failed to delete item from cart"]);
       returnJsonHttpResponse(500, $response);
     }
+    break;
 }
+?>
